76b72acb0a3c1967786781ccc2db0b5fdcbde5af
John Bieling <john.bieling@gmx.de>
2021-09-15 11:55:16 +0200

    upd

--- add-on-reports-generator/build_reports.js
+++ add-on-reports-generator/build_reports.js
@@ -22,7 +22,7 @@ const badge_definitions = {
 }
 
 const wip = {
-	986686: "https://github.com/thundernest/import-export-tools-ng/pull/268",
+	986686: "https://github.com/thundernest/import-export-tools-ng/issues/264",
 	90003: "https://github.com/cleidigh/Localfolder-TB/pull/51",
 	310: "https://github.com/eyalroz/bidimailui/tree/development",
 	331319: "-", //Folder Pane Switcher
