2cf7b1c033c35b97ff2c9375f4b6af4fe304e2a4
John Bieling <john.bieling@gmx.de>
2023-07-24 09:35:44 +0200

    upd 24.07

--- add-on-reports-generator/build_reports.js
+++ add-on-reports-generator/build_reports.js
@@ -215,7 +215,6 @@ const wip115 = [
     "986258", // Provider for DAV
     "986338", // Provider for Exchange
     "987840", // PrintingTools NG
-    "4654", //addon/removedupes - needs to adapt to mail:3pane, could use menus API - contacted
     "3254", //addon/quickfolders-tabbed-folders/
 ]
 const column115 = [
